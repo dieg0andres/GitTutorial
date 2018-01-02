fb139bf132a9527481a2665a7e22ea792de662c4
Diego Galindo <diego.a.galindo@gmail.com>
2018-01-02 07:41:22 -0600

    back to normal

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -11,9 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        print("i'm tired of hellos")
-        print("hello world")
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
 }
 
